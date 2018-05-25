15839aabe8b61d7bcf626c5960c2e16b85817d7a
t.goto <t.goto@chatbox-inc.com>
2018-05-26 00:54:46 +0900

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Github Issue API 
+
+Issue の状態に応じて、画像を返却してくれます。
+
+スプレッドシートなどで以下のように `image` 関数を利用して Issue の状態を表示できます。
+
+```
+```
+=image("https://cb-ghissues.herokuapp.com/chatbox-inc/gh-issues/issues/1"&"?time="&now())
+```
---
+++ modules/issues/src/Http/Actions/IssueListAction.php
@@ -0,0 +1,43 @@
+<?php
+namespace Chatbox\GithubIssues\Http\Action;
+use Chatbox\GithubIssues\Service\GithubService;
+use GuzzleHttp\Exception\ClientException;
+
+/**
+ * Created by PhpStorm.
+ * User: mkkn
+ * Date: 2018/05/25
+ * Time: 23:09
+ */
+class IssueListAction {
+
+    protected $github;
+
+    /**
+     * IssueImageAction constructor.
+     *
+     * @param $github
+     */
+    public function __construct(GithubService $github ) {
+        $this->github = $github;
+    }
+
+
+    public function __invoke($username,$reponame,$issueno) {
+        try{
+            $result = $this->github->issue($username,$reponame,$issueno);
+        }catch (ClientException $e){
+            return  response()->file(app()->basePath("resources/assets/images/default.png"));
+        }
+        if(is_array($result)){
+            if(array_get($result,"state") === "open"){
+                return  response()->file(app()->basePath("resources/assets/images/open.png"));
+            }else if(array_get($result,"state") === "closed"){
+                return  response()->file(app()->basePath("resources/assets/images/close.png"));
+            }
+        }
+        return  response()->file(app()->basePath("resources/assets/images/default.png"));
+    }
+
+
+}
